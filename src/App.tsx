a1feeadd01d7b2c17d44ec55d1c3439d4839b068
deniisolo <deniisolo@outlook.es>
2024-07-15 16:21:16 -0500

    feat(UI): add a Orderview component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Login } from "./components/views/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Waiterview } from "./components/views/Waiterview";
 import { Chefview } from "./components/views/Chefview";
+import { Ordersview } from "./components/views/Ordersview";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/waiterview" element={<Waiterview />} />
         <Route path="/chefview" element={<Chefview />} />
+        <Route path="/orderview" element={<Ordersview />} />
       </Routes>
     </Router>
   );
---
+++ src/components/views/Ordersview.tsx
@@ -0,0 +1,14 @@
+import "./Ordersview.css";
+import { Navbar } from "../Navbar";
+export function Ordersview() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <h3>Historial de pedidos</h3>
+      </main>
+    </>
+  );
+}
